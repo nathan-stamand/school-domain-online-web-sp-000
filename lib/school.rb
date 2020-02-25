ae4dcdf61b73591fe321311b87e7a03826bc990a
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-25 15:15:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,14 +4,14 @@ class School
   def initialize(name)
     @name = name 
     def roster 
-      @@roster = {}
-      return @@roster
+      roster = {}
+      return roster
     end
   end
   
   def add_student(student, grade)
-    if @@roster.keys.none?(grade)
-      @@roster[grade] = [student]
+    if roster.keys.none?(grade)
+      roster[grade] = [student]
     end
   end
   
